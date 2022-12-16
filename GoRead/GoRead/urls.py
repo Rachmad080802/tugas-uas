209ce6b8c868ad813afb25df81657cedface6279
Rachmad080802 <107178078+Rachmad080802@users.noreply.github.com>
2022-12-16 23:18:14 +0700

    Update urls.py
    menambahkan url dan import views

--- GoRead/GoRead/urls.py
+++ GoRead/GoRead/urls.py
@@ -19,8 +19,13 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from upload import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',index, name='index'),
+    path('signup/', views.SignUp.as_view(), name='signup'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
