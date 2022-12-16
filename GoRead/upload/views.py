a115d1be8a4f50ddd52d87aacdd42a0f1889080d
Rachmad080802 <107178078+Rachmad080802@users.noreply.github.com>
2022-12-16 23:25:24 +0700

    Update views.py
    menambahkan UserCreationForm di dalam views

--- GoRead/upload/views.py
+++ GoRead/upload/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from upload.models import Upload
+from django.urls import reverse_lazy
+from django.views import generic
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def index(request):
@@ -7,3 +10,8 @@ def index(request):
     posting = Upload.objects.all()
 
     return render(request, 'upload/index.html', {'posting':posting} )
+class SignUp(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
+
