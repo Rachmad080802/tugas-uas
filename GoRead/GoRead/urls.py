7f0c23f837a5d2998ea6527ea87e32e8dfaed9a2
Rachmad080802 <107178078+Rachmad080802@users.noreply.github.com>
2022-12-20 14:02:15 +0700

    Update urls.py
    menambahkan url contact

--- GoRead/GoRead/urls.py
+++ GoRead/GoRead/urls.py
@@ -28,4 +28,5 @@ urlpatterns = [
     path('signup/', views.SignUp.as_view(), name='signup'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('contact', views.KontakViewsClass.as_view(), name='contact'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
