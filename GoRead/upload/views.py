9564a37f280e6a420b5d472bc06386dbb56bd66f
Rachmad080802 <107178078+Rachmad080802@users.noreply.github.com>
2022-12-20 14:08:05 +0700

    Update views.py
    menambahkan class dan from import

--- GoRead/upload/views.py
+++ GoRead/upload/views.py
@@ -3,6 +3,8 @@ from upload.models import Upload
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
+from .models import KontakModel
+from .forms import KontakFormModel
 
 # Create your views here.
 def index(request):
@@ -14,4 +16,8 @@ class SignUp(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
+    
 
+class KontakViewsClass(CreateView):
+    form_class = KontakFormModel
+    template_name = 'contact_us/contact.html'
